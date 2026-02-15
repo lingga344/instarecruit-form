98d2530d35edeb93a8c2638864c2af041f02f3b3
lingga344 <digitalforensics.id@gmail.com>
2026-02-15 19:49:24 +0700

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Hero } from './components/Hero';
-import { RecruitmentForm } from './components/RecruitmentForm';
+import { Hero } from '@/components/Hero';
+import { RecruitmentForm } from '@/components/RecruitmentForm';
 
 const App: React.FC = () => {
   return (
